140de6648f92390f28d47cb63d96afd299039493
federicomanni97 <manni_federico@libero.it>
2024-08-23 14:31:36 +0200

    days setup

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   <header>
     <div class="container">
       <h1 class="text-center text-light">Trips Diary</h1>
-      <div class="calendar">
+      <div class="calendar bg-light p-3 m-auto">
         <header>
           <h3></h3>
           <nav>
@@ -14,14 +14,14 @@
           </nav>
         </header>
         <section class="bg-light">
-          <ul class="days">
-            <li>Sunday</li>
-            <li>Monday</li>
-            <li>Tuesday</li>
-            <li>Wednesday</li>
-            <li>Thursday</li>
-            <li>Friday</li>
-            <li>Saturday</li>
+          <ul class="days fs-5 d-flex justify-content-between text-align-center list-unstyled">
+            <li class="p-3">Sunday</li>
+            <li class="p-3">Monday</li>
+            <li class="p-3">Tuesday</li>
+            <li class="p-3">Wednesday</li>
+            <li class="p-3">Thursday</li>
+            <li class="p-3">Friday</li>
+            <li class="p-3">Saturday</li>
           </ul>
           <ul class="dates"></ul>
         </section>
