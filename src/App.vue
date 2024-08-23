14dbac414d95358e5e03c8f02071ee9c74699b80
federicomanni97 <manni_federico@libero.it>
2024-08-23 11:52:14 +0200

    setup

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,21 @@
 
 <template>
   <header>
-    <h1>test</h1>
+    <div class="container">
+      <h1 class="text-center">Trip Diary</h1>
+      <div></div>
+    </div>
   </header>
 
   <main>
+    <div>
+      
+    </div>
   </main>
 </template>
+
+<script>
+</script>
 
 <style scoped>
 
