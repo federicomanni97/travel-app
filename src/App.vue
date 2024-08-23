6c8573623cb38bfbbbe652998a9a44ab4d97e3df
federicomanni97 <manni_federico@libero.it>
2024-08-23 14:26:29 +0200

    calendar setup

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,29 @@
 <template>
   <header>
     <div class="container">
-      <h1 class="text-center">Trip Diary</h1>
-      <div></div>
+      <h1 class="text-center text-light">Trips Diary</h1>
+      <div class="calendar">
+        <header>
+          <h3></h3>
+          <nav>
+            <button id="previous"></button>
+            <button id="next"></button>
+          </nav>
+        </header>
+        <section class="bg-light">
+          <ul class="days">
+            <li>Sunday</li>
+            <li>Monday</li>
+            <li>Tuesday</li>
+            <li>Wednesday</li>
+            <li>Thursday</li>
+            <li>Friday</li>
+            <li>Saturday</li>
+          </ul>
+          <ul class="dates"></ul>
+        </section>
+
+      </div>
     </div>
   </header>
 
