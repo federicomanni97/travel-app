3dc82a3e1361e0d293ff722d58440f73f11f6391
federicomanni97 <manni_federico@libero.it>
2024-08-29 02:36:51 +0200

    fixed javascript calendar + calendar component

--- src/App.vue
+++ src/App.vue
@@ -1,176 +1,29 @@
 <template>
-  <header>
-    <div class="container border-rounded">
-      <h1 class="text-center text-light my-5 text-uppercase">Trips Diary</h1>
-      <div class="calendar my-5 py-5 bg-light p-3 m-auto rounded-4">
-        <header>
-          <div class="d-flex justify-content-end flex-column align-items-end mb-4">
-            <nav>
-              <button id="previous" class="btn btn-light fs-3 text-black" @click="prevMonth"><</button>
-              <button id="next" class="btn btn-light fs-3 text-black" @click="nextMonth">></button>
-            </nav>
-            <h3 class="text-uppercase month fs-1 fw-bold"> {{ monthName }} {{ year }}</h3>
-          </div>
-        </header>
-        <section class="">
-          <ul class="days list-unstyled fs-3 text-uppercase">
-            <li class="">Sunday</li>
-            <li class="">Monday</li>
-            <li class="">Tuesday</li>
-            <li class="">Wednesday</li>
-            <li class="">Thursday</li>
-            <li class="">Friday</li>
-            <li class="">Saturday</li>
-          </ul>
-          <ul class="dates list-unstyled fs-2" id="dates" ref="dates">
-            <li class="p-3" @click="openModal(i)">{{ i }}</li>
-          </ul>
-        </section>
-      </div>
-      <div>
-        <h1 class="text-light" @click="() => toggleModal('buttonTrigger')">my modal component</h1>
-        <!-- <button @click="ToggleModal('buttonTrigger')">Open ModalComponent</button> -->
-        <ModalComponent v-if="modalTriggers.buttonTrigger"
-        :toggleModal="() => toggleModal('buttonTrigger')">
-          <h2> My ModalComponent</h2>
-        </ModalComponent>
-      </div>
-    </div>
+  <div class="container h-100">
+  <header class="text-center my-5">
+    <h1 class="text-light">Travel Trips</h1>
   </header>
-
   <main>
     <div>
-      
+      <CalendarComponent/>
     </div>
   </main>
+</div>
 </template>
 
 <script>
-// primo
-import { ref } from 'vue';
-import ModalComponent from './components/ModalComponent.vue';
-// import Modal from './components/ModalComponent.vue'
+
+import CalendarComponent from './components/CalendarComponent.vue';
+
 export default {
-  data() {
-    const modalTriggers = ref({
-      buttonTrigger: false,
-      timedTrigger: false
-    });
-
-    const toggleModal = (trigger) => {
-      modalTriggers.value[trigger] = !modalTriggers.value[trigger];
-    }
-
-    return {
-      ModalComponent,
-      modalTriggers,
-      toggleModal,
-      month: new Date().getMonth(),
-      year: new Date().getFullYear(),
-      showModal: false,
-      monthNames: [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-      ],
-    };
-  },
-
   components: {
-    ModalComponent
+    CalendarComponent
   },
+}
 
-  computed: {
-    monthName() {
-      return this.monthNames[this.month];
-    }
-  },
-
-  methods: {
-    prevMonth() {
-      this.month--;
-      if (this.month < 0) {
-        this.month = 11;
-        this.year--;
-      }
-      this.renderCalendar();
-    },
-
-    nextMonth() {
-      this.month++;
-      if (this.month > 11) {
-        this.month = 0;
-        this.year++;
-      }
-      this.renderCalendar();
-    },
-
-    renderCalendar() {
-      // Calculate start and end dates
-      const start = new Date(this.year, this.month, 1).getDay();
-      const endDate = new Date(this.year, this.month + 1, 0).getDate();
-      
-      // Render dates
-      let datesHtml = '';
-      
-      for (let i = 0; i < start; i++) {
-      datesHtml += `<li></li>`;
-      }
-
-      for (let i = 1; i <= endDate; i++) {
-        datesHtml += `<li class="p-3">${i}</li>`;
-      }
-
-      this.$refs.dates.innerHTML = datesHtml;
-    },
-    toggleModal(trigger) {
-      this.modalTriggers[trigger] = !this.modalTriggers[trigger];
-    },    
-  },
-
-  mounted() {
-    this.renderCalendar();
-  }
-};
 
 </script>
 
 <style scoped>
-.calendar ul li{
-  display: grid;
-  
-}
-
-.calendar-grid {
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-}
-
-.days, .dates {
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  justify-items: center;
-}
-
-#previous:hover, #next:hover{
-  background-color: lightblue;
-} 
-
-.days{
-  color: lightblue;
-}
-
-.month{
-  color: lightblue;
-}
 
 </style>
---
+++ src/components/CalendarComponent.vue
@@ -0,0 +1,229 @@
+<template>
+     <div class="calendar text-light h-100">
+        <div class="d-flex justify-content-end flex-column align-items-end mb-4">
+            <nav>
+                <button id="previous" class="btn style-button fs-3 text-black" @click="prevMonth"><</button>
+                <button id="next" class="btn style-button fs-3 text-black" @click="nextMonth">></button>
+            </nav>
+            <h3 class="text-uppercase month fs-1 fw-bold"> {{ getMonthName(selectedMonth) }} {{ selectedMonthYear.year }}</h3>
+        </div>
+        <div class="days list-unstyled fs-3 text-uppercase" >
+            <ul class="days" v-for="(day, index) in dayNames" :key="`day-${index}`">
+                <li class="rounded-4 ">{{ day }}</li>
+            </ul>
+        </div>
+        <ul class="dates list-unstyled rounded-10 w-100 fs-1" id="dates" >
+            <li class="number square rounded-circle fs-2" v-for="(day, index) in getDays(selectedMonthYear.year, selectedMonthYear.month)"
+            :key="`day-${index}`" @click="() => toggleModal('buttonTrigger')">
+                {{ day.number }}
+            </li>
+        </ul>
+    </div>
+    <div>
+        <ModalComponent v-if="modalTriggers.buttonTrigger"
+        :toggleModal="() => toggleModal('buttonTrigger')">
+            <h2> My ModalComponent</h2>
+        </ModalComponent>
+    </div>
+</template>
+
+<script>
+
+import { ref } from 'vue';
+import ModalComponent from './ModalComponent.vue';
+
+export default {
+name: 'CalendarComponent',
+data() {
+        const modalTriggers = ref({
+            buttonTrigger: false,
+            timedTrigger: false
+        })
+        const toggleModal = (trigger) => {
+        modalTriggers.value[trigger] = !modalTriggers.value[trigger];
+        }
+      return {
+        ModalComponent,
+        modalTriggers,
+        toggleModal,
+        showModal: false,
+        downloading: false,
+        selectedMonth: null,
+        selectedYear: null,
+        showMonthSelection: false,
+        dayNames: [
+          'Saturday',
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday'
+        ],
+        monthNames: [
+          { month: 1, title: 'January' },
+          { month: 2, title: 'February' },
+          { month: 3, title: 'March' },
+          { month: 4, title: 'April' },
+          { month: 5, title: 'May' },
+          { month: 6, title: 'June' },
+          { month: 7, title: 'July' },
+          { month: 8, title: 'August' },
+          { month: 9, title: 'September' },
+          { month: 10, title: 'October' },
+          { month: 11, title: 'November' },
+          { month: 12, title: 'December' }
+        ]
+      }
+    },
+    components: {
+      ModalComponent
+    },
+    computed: {
+      startOfMonthDay() {
+        const date = this.selectedMonthYear
+        const firstDay = new Date(date.year, date.month - 1, 1) // gets first day of the selected month
+        return firstDay.getUTCDay()
+      },
+      currentMonthYear() {
+        const date = new Date()
+        const month = date.getMonth() + 1
+        const Year = date.getFullYear()
+        return { month: month, year: Year }
+      },
+      selectedMonthYear() {
+        const date = new Date()
+        let month
+        let year
+        if (this.selectedMonth != null) {
+          month = this.selectedMonth
+        }
+        else {
+          month = date.getMonth() + 1
+        }
+        if (this.selectedYear != null) {
+          year = this.selectedYear
+        }
+        else {
+          year = date.getFullYear()
+          this.selectedYear = year  
+        }
+        return { month: month, year: year }
+      }
+    },
+    created() {
+      if (this.selectedMonth === null) {
+        this.selectedMonth = this.selectedMonthYear.month
+      }
+    },
+    methods: {
+      updateSelectedYear(year) {
+        // this.selectedYear = year
+      },
+      updateSelectedMonth(month) {
+        this.selectedMonth = month
+        this.showMonthSelection = false
+      },
+      doSomething() {
+        alert('Doing something')
+      },
+      getMonthName(month) {
+        return this.monthNames.find(item => item.month === month).title
+      },
+      getCashupReports() {
+        this.$store.dispatch('getCashUpRecords')
+      },
+      getDays(year, month) {
+        const date = new Date(year, month - 1, 1)
+        const result = []
+        let dayOffset = 2 + this.startOfMonthDay
+        if (dayOffset === 7) {
+          dayOffset = 7 - dayOffset
+        }
+        let i
+        for (i = 0; i < dayOffset; i++) {
+          result.push({ number: null, date: null, fullDate: null })
+        }
+
+        while (date.getMonth() === month - 1) {
+          const dd = String(date.getDate()).padStart(2, '0')
+          const mm = String(date.getMonth() + 1).padStart(2, '0')
+          const yyyy = date.getFullYear()
+          const shortDate = `${yyyy},${mm},${dd}`
+          const item = { number: date.getDate(), date: shortDate, fullDate: date.toString() }
+          result.push(item)
+          date.setDate(date.getDate() + 1)
+        }
+        return result
+      }, 
+      prevMonth() {
+        if (this.selectedMonth === 1) {
+          this.selectedMonth = 12
+          this.selectedYear = this.selectedYear - 1
+        }
+        else {
+          this.selectedMonth = this.selectedMonth - 1
+        }
+      },
+      nextMonth() {
+        if (this.selectedMonth === 12) {
+          this.selectedMonth = 1
+          this.selectedYear = this.selectedYear + 1
+        }
+        else {
+          this.selectedMonth = this.selectedMonth + 1
+        }
+      },
+      toggleModal(trigger) {
+        this.modalTriggers[trigger] = !this.modalTriggers[trigger];
+      },    
+    }
+}
+</script>
+<style scoped>
+
+.dates li:hover{
+  background-color: lightblue;
+  cursor: pointer;
+}
+
+.calendar ul li{
+  display: grid;
+}
+
+.calendar-grid {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+}
+
+.days, .dates {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  justify-items: center;
+}
+
+#previous:hover, #next:hover{
+  background-color: lightblue;
+} 
+
+.days{
+  color: lightblue;
+}
+
+.month{
+  color: lightblue;
+}
+
+.square li{
+    height: 80px;
+    width: 80px;
+    display: grid;
+    place-items: center;
+}
+
+.style-button{
+    background-color: transparent;
+    border: 0px;
+}
+
+</style>
